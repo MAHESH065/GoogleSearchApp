5163b1a4383a6aec5075b886784aa52e87efd183
Mahesh <maheshwar.4ever@gmail.com>
2021-09-22 20:02:24 +0530

    initial commit

---
+++ src/main/java/com/jsr/GoogleSearchAppApplication.java
@@ -0,0 +1,13 @@
+package com.jsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSearchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleSearchAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsr/GoogleSearchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleSearchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
